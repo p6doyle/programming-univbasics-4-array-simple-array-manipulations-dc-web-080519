e599a7b7356a9a51264a0838820089ebd730deeb
Peyton Doyle <p6doyle@gmail.com>
2019-07-17 13:28:08 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -28,11 +28,7 @@ def shift_with_args(array)
 end
 
 def using_concat(array)
-  my_favorite_things = ["raindrops on roses", "whiskers on kittens"]
-  more_favs = ["sports cars", "flatiron school"]
-  all_my_favs = my_favorite_things.concat(more_favs)
-  p my_favorite_things
-  p all_my_favs
+  my_favorite_things.concat(more_favs)
 end
 
 def using_insert(array, element)
